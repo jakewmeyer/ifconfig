e40a0d1a426e1e4d7ecd91909270db9c4503d44c
Jake Meyer <jakewmeyer@gmail.com>
2022-04-19 20:25:08 -0500

    use 7000 as default port

--- src/main.rs
+++ src/main.rs
@@ -15,7 +15,7 @@ async fn main() -> Result<()> {
     dotenv::dotenv().ok();
     tracing_subscriber::fmt::init();
     let host = std::env::var("HOST").unwrap_or_else(|_| "127.0.0.1".to_string());
-    let port = std::env::var("PORT").unwrap_or_else(|_| "7001".to_string());
+    let port = std::env::var("PORT").unwrap_or_else(|_| "7000".to_string());
     let addr = format!("{}:{}", host, port);
     HttpServer::new(|| {
         App::new()
