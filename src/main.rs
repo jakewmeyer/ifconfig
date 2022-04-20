e3a264a0e7849e2a1e457c00397b83aa35d9f399
Jake Meyer <jakewmeyer@gmail.com>
2022-04-19 20:16:47 -0500

    use 7001 as default port

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ use actix_web::{
     middleware::{Compress, NormalizePath},
     App, HttpServer,
 };
-use miette::{Result, IntoDiagnostic};
+use miette::{IntoDiagnostic, Result};
 use tracing_actix_web::TracingLogger;
 
 mod errors;
@@ -15,7 +15,7 @@ async fn main() -> Result<()> {
     dotenv::dotenv().ok();
     tracing_subscriber::fmt::init();
     let host = std::env::var("HOST").unwrap_or_else(|_| "127.0.0.1".to_string());
-    let port = std::env::var("PORT").unwrap_or_else(|_| "7100".to_string());
+    let port = std::env::var("PORT").unwrap_or_else(|_| "7001".to_string());
     let addr = format!("{}:{}", host, port);
     HttpServer::new(|| {
         App::new()
@@ -25,8 +25,10 @@ async fn main() -> Result<()> {
             .service(routes::get_ip_plaintext)
             .service(routes::get_ip_json)
     })
-    .bind(addr).into_diagnostic()?
+    .bind(addr)
+    .into_diagnostic()?
     .run()
-    .await.into_diagnostic()?;
+    .await
+    .into_diagnostic()?;
     Ok(())
 }
